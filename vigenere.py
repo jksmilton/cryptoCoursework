b62b5e0423466f2ed3bdf2b1116286c2ebe69fff
Jordan Milton <jksmilton@gmail.com>
2014-03-31 19:48:20 +0100

    made improvements to IC, added function that gets best candidates

--- vigenere.py
+++ vigenere.py
@@ -3,22 +3,25 @@ def incidentOfCoincidence(text, step):
 		('h',0), ('i',0), ('j',0), ('k',0), ('l',0), ('m',0), ('n',0), ('o',0),
 		('p',0), ('q',0), ('r',0), ('s',0), ('t',0), ('u',0), ('v',0), ('w',0),
 		('x',0), ('y',0), ('z',0)])
-	i = 0
-	n = 0
+	n, start, incidence = 0, 0, 0
+	while start < step:
+		i = start
+		while i < len(text):
+			mapOfCounts[text[i]] = mapOfCounts[text[i]] + 1
+			i = i + step
+			n = n + 1
 
-	while i < len(text):
-		mapOfCounts[text[i]] = mapOfCounts[text[i]] + 1
-		i = i + step
-		n = n + 1
+		sumOfcounts = 0
+		for v in mapOfCounts.values():
+			sumOfcounts = v * (v - 1)
 
-	sumOfcounts = 0
+		divisor = n * (n - 1) / 26
+		sumOfcounts = sumOfcounts / divisor
+		incidence = incidence + sumOfcounts
+		start = start + 1
+	incidence = incidence / step
 
-	for v in mapOfCounts.values():
-		sumOfcounts = v * (v - 1)
-
-	divisor = n * (n - 1) / 26
-	sumOfcounts = sumOfcounts / divisor
-	return sumOfcounts
+	return incidence
 
 def getStrippedFile(fileName):
 	f = open(fileName, 'r')
@@ -26,3 +29,15 @@ def getStrippedFile(fileName):
 	while(not(f.closed)):
 		f.close()
 	return output.replace(" ", "")
+
+def getKeyLengthCandidates(text, numToTest):
+	maxKeyLength = len(text) / 2
+	results = dict()
+	i = 1
+	while i < maxKeyLength:
+		results[i] = incidentOfCoincidence(text, i)
+		i = i + 1
+	sortedResults = sorted(results.items(), key=lambda x: x[1])
+	keyLengthsToTest, notWanted = zip(*(sortedResults[-numToTest:]))
+	return keyLengthsToTest, notWanted
+
